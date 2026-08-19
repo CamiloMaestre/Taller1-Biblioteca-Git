833d19cde24fa86d25bce2b2325cff311df33eb4
Camilo Andres Maestre Davila <camaestredavila@unicesar.edu.co>
2026-08-18 20:40:48 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,17 +1,28 @@
 package com.mycompany.biblioteca;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    static ArrayList<Client> clients = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    static void addClient() {
+        System.out.println("Ingrese el ID del cliente:");
+        String id = sc.nextLine();
+        System.out.println("Ingrese el nombre del cliente:");
+        String name = sc.nextLine();
+        System.out.println("Ingrese el celular del cliente:");
+        String cellphone = sc.nextLine();
+        System.out.println("Ingrese el correo electrónico del cliente:");
+        String email = sc.nextLine();
+
+        Client client = new Client(id, name, cellphone, email);
+        clients.add(client);
+    }
+
+    public static void main(String[] args) {
+
     }
 }
